f19dfe52aaec311ed48a53572670e4e7d4a2841b
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-04-02 15:13:23 -0300

    first commit

---
+++ api-golang-test/configs/config.go
@@ -0,0 +1,23 @@
+package configs
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Pass     string
+	Database string
+}
+
+func init() {
+
+}
---
+++ api-golang-test/go.mod
@@ -0,0 +1,3 @@
+module api-golang-test
+
+go 1.22.1
